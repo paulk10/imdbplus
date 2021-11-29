7948d244d02fd4f13aacf0dafc1e584793b7833a
paulk10 <38840579+paulk10@users.noreply.github.com>
2021-11-29 11:21:52 +0100

    added agerating to Dune movie

--- components/Movie.js
+++ components/Movie.js
@@ -54,6 +54,9 @@ const Movie = ({ data, level }) => {
     var genres = data.rels.filter(obj => {
       return content.genres.includes(obj.uuid);
     })
+    var ageratings = data.rels.filter(obj => {
+      return content.agerating.includes(obj.uuid);
+    })
   } else {
     var content = data;
   }
@@ -83,6 +86,13 @@ const Movie = ({ data, level }) => {
           </h1>
           <div className={styles.genrelist}>
             {genres.map((item, index) => (
+              <div className={styles.genre}>
+                {item.content.title}
+              </div>
+            ))}
+          </div>
+          <div className={styles.genrelist}>
+            {ageratings.map((item, index) => (
               <div className={styles.genre}>
                 {item.content.title}
               </div>
